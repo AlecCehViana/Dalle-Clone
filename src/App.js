a9f5f1eb9efc10e8beb7638092b44a1f9e25f2d0
AlecCehVina <aleccehviana@gmail.com>
2023-09-05 00:57:15 -0600

    Dalle Clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [images, setImages] = useState(null);
+  const [value, setValue] = useState(null);
+
+  const surpriseOptions = [
+    "A blue ostrich eating melon",
+    "A matisse style shark on the telephone",
+    "A pineapple sunbathing on an island",
+  ];
+  const surpriseMe = () => {
+    setImages(null);
+    const randomValue =
+      surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)];
+    setValue(randomValue);
+  };
+
+  const getImages = async () => {
+    setImages(null);
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          message: value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await fetch("http://localhost:8000/images", options);
+      const data = await response.json();
+      console.log(data);
+      setImages(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <section className="search-section">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Start with a detailed description : 
+          <span className="surprise" onClick={surpriseMe}>
+            
+            Surprise me
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="input-container">
+          <input
+            value={value}
+            placeholder="An impressionist oil painting of a sunflower in a purple vase..."
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button onClick={getImages}> Create </button>
+        </div>
+      </section>
+
+      <section className="image-section">
+        {images?.map((image, _index) => (
+          <img
+            key={_index}
+            src={image.url}
+            alt={`generated image of ${value}`}
+          />
+        ))}
+      </section>
     </div>
   );
-}
+};
 
 export default App;
